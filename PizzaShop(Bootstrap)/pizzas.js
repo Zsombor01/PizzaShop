c53f96c296193317ed5825ee16a72f1c3566098b
Zsombor01 <zs.zsombor.biro@gmail.com>
2022-09-13 23:36:26 +0200

    Version 1

---
+++ PizzaShop(Bootstrap)/pizzas.js
@@ -0,0 +1,43 @@
+const pizzas = [
+    {
+        id: 1,
+        name: 'Sonkás-kukoricás',
+        price: 1000,
+    },
+    {
+        id: 2,
+        name: 'Sonkás-gombás',
+        price: 1000,
+    },
+    {
+        id: 3,
+        name: 'Sonkás-sajtos',
+        price: 1000,
+    },
+    {
+        id: 4,
+        name: 'Berlin',
+        price: 1200,
+    },
+    {
+        id: 5,
+        name: 'Frankfurt',
+        price: 1300,
+    },
+    {
+        id: 6,
+        name: 'Krakow',
+        price: 1100,
+    },
+    {
+        id: 7,
+        name: 'Magyaros',
+        price: 1250,
+    },
+    {
+        id: 8,
+        name: 'Gyrosos',
+        price: 1400,
+    },
+];
+
